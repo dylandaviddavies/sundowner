32f63b8aafc478bbde02c4f7c0fb4f3eb8f4d1d8
Dylan Davies <dylandaviddavies@gmail.com>
2020-11-01 12:32:40 -0500

    Add theme switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sundowner';
+  theme = 'light';
+
+  switchTheme(): void {
+    switch (this.theme) {
+      case 'light':
+        this.theme = 'dark';
+        document.documentElement.setAttribute('data-theme', this.theme);
+        break;
+      case 'dark':
+        this.theme = 'light';
+        document.documentElement.setAttribute('data-theme', this.theme);
+        break;
+    }
+  }
 }
